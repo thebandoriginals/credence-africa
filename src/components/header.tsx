7d8bc84b596531d848a01c3f15197a15c2d3eadb
The Band <thebandoriginals@gmail.com>
2025-10-03 16:00:39 +0000

    Make

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -73,7 +73,7 @@ export function Header({insights}: {insights: Insight[]}) {
     <header className="sticky top-0 z-50 w-full border-b bg-white">
       <div className="container flex h-20 items-center justify-between">
         <Link href="/" className="flex items-center gap-2 font-bold text-xl">
-          <Logo className="h-[200px] w-auto"/>
+          <Logo className="h-[170px] md:h-[200px] w-auto"/>
         </Link>
         <div className="hidden lg:flex flex-grow items-center justify-center">
           <NavigationMenu>

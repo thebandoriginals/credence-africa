c8bd5c27e654feceb54a32acd91860fd58f1ffed
The Band <thebandoriginals@gmail.com>
2025-10-03 18:56:14 +0000

    Changes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,10 @@
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Image
+    <img
       src="/logo.png"
       alt="Credence Africa logo"
-      width={100}
-      height={100}
       className={cn("h-10 w-10", className)}
     />
   );
